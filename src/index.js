0c36818cc22ab27d247cb863c838b3d4923a7f7b
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-08 15:12:13 -0600

    dog imgs populate dog-image-container

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 console.log('%c HI', 'color: firebrick')
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
 
 
 document.addEventListener("DOMContentLoaded", function() {
-
+    fetchImgs();
 });
 
 function fetchImgs() {
     fetch(imgUrl)
     .then( res => res.json() )
-    .then( res => )
+    .then(addImgToDiv)
 }
 
- 
+function addImgToDiv(json) {
+    const dogDiv = document.querySelector("div#dog-image-container");
+    const dogImgs = json["message"];
+    for ( const element of dogImgs) {
+        let dogImg = document.createElement("img");
+        dogImg.src = element;
+        dogDiv.appendChild(dogImg);
+    };
+};
