064f64e11b788af59ae84da95a3c63b557e2e70f
github username <github email address>
2020-01-08 13:51:10 -0600

    for loop is integrated for img url

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-
-document.addEventListener("DOMContentLoaded", function() {
-    fetchImg()
-});
+const createImg = document.createElement('img')
+const findDivImg = document.getElementById('dog-image-container')
 
 function fetchImg() {
     return fetch(imgUrl)
     .then(resp => resp.json())
-    .then(json => console.log(json['message']));
+    .then(json => loopingObject(json['message']));
 }
+
+function loopingObject(obj){
+    // for(let i = 0; i < obj.length; i++) {
+    //     console.log(obj[i])
+    // }
+    
+    // for (const key in obj){
+    //     console.log(obj[key])
+    // }
+
+    for (const val of obj){
+        console.log(val)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetchImg()
+});
