dab2857f35647788fa0f98057113f40898118ae7
hfarnsworth <havilahfarnsworth@gmail.com>
2020-01-08 13:17:12 -0600

    starting out

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+
+document.addEventListener("DOMContentLoaded", function() {
+
+});
+
+function fetchImgs() {
+    fetch(imgUrl)
+    .then( res => res.json() )
+    .then( res => )
+}
+
+ 
